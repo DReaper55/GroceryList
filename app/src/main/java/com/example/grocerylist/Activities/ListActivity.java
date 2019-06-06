56add80b5e5da89f50043f76d8bb97b4d5cd3be9
Daniel <danieluwadi@yahoo.com>
2019-06-06 17:18:58 +0100

    Initial commit

--- app/src/main/java/com/example/grocerylist/Activities/ListActivity.java
+++ app/src/main/java/com/example/grocerylist/Activities/ListActivity.java
@@ -1,11 +1,7 @@
 package com.example.grocerylist.Activities;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
--- app/src/main/java/com/example/grocerylist/UI/MyAdapter.java
+++ app/src/main/java/com/example/grocerylist/UI/MyAdapter.java
@@ -3,14 +3,19 @@ package com.example.grocerylist.UI;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.grocerylist.Activities.DetailsActivity;
+import com.example.grocerylist.Data.DatabaseHandler;
 import com.example.grocerylist.Model.Grocery;
 import com.example.grocerylist.R;
 
@@ -19,6 +24,9 @@ import java.util.List;
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     private Context context;
     private List<Grocery> groceries;
+    private EditText enterItem;
+    private EditText enterQty;
+    private AlertDialog dialog;
 
     public MyAdapter(Context context, List<Grocery> groceries) {
         this.context = context;
@@ -93,13 +101,79 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         public void onClick(View v) {
             switch (v.getId()){
                 case R.id.editID:
+                    int position = getAdapterPosition();
+                    Grocery grocery1 = groceries.get(position);
+
+                    setUpAlert(grocery1);
 
                     break;
 
                 case R.id.deleteID:
+                    int pos = getAdapterPosition();
+                    Grocery grocery = groceries.get(pos);
 
+                    deleteItem(grocery.getId());
+
+                    Toast.makeText(v.getContext(), "item removed", Toast.LENGTH_LONG).show();
                     break;
             }
         }
+
+        private void deleteItem(int id){
+            DatabaseHandler db = new DatabaseHandler(context);
+            int pos = getAdapterPosition();
+
+            db.deleteGrocery(id);
+            groceries.remove(pos);
+            notifyItemRemoved(pos);
+
+        }
+
+
+        private void setUpAlert(final Grocery grocery){
+            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View view = inflater.inflate(R.layout.add_item, null);
+
+            enterItem = view.findViewById(R.id.groceryItem);
+            enterQty = view.findViewById(R.id.groceryQty);
+            Button saveButton = view.findViewById(R.id.grocerySave);
+
+            alertBuilder.setView(view);
+            dialog = alertBuilder.create();
+
+            dialog.show();
+
+            saveButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    if(!enterItem.getText().toString().isEmpty() && !enterQty.getText().toString().isEmpty()){
+
+
+                        updateDB(grocery);
+                        dialog.dismiss();
+                    }
+                }
+            });
+
+
+        }
+
+        private void updateDB(Grocery grocery){
+            DatabaseHandler db = new DatabaseHandler(context);
+            int pos = getAdapterPosition();
+
+            grocery.setName(enterItem.getText().toString());
+            grocery.setQuantity(enterQty.getText().toString());
+
+            db.updateGrocery(grocery);
+
+            notifyItemChanged(pos);
+
+        }
     }
+
+
+
 }
